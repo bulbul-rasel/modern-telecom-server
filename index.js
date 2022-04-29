9aab3ac037e3d1dc5b0086b76ab194a4a14e48cd
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-04-29 15:14:38 +0600

    delete api done

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const validateId = (req, res, next) => {
+    const id = req.params.id;
+    const objectIdRegex = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
+    const validId = objectIdRegex.test(id);
+
+    if (!id || !validId) {
+        return res.send({ success: false, error: 'Invalid id' });
+    }
+
+    req.id = id;
+
+    next();
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqtkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -48,6 +62,23 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         })
+
+        // delete
+        app.delete("/products/:id", validateId, async (req, res) => {
+            const id = req.id;
+
+
+            const result = await productCollection.deleteOne({ _id: ObjectId(id) })
+
+            console.log(result)
+
+            if (!result.deletedCount) {
+                return res.send({ success: false, error: "something went wrong" });
+            }
+
+            res.send({ success: true, message: "Successfully deleted " })
+
+        });
     }
     catch (error) {
         console.log(error);
