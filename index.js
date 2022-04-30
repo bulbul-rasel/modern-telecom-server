a91631e62dde6a55d1963eeb36e819ce35c5b0d2
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-05-01 02:41:34 +0600

    jwt token for email done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,23 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorize Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+    next();
+}
 
 const validateId = (req, res, next) => {
     const id = req.params.id;
@@ -34,6 +52,15 @@ async function run() {
         await client.connect();
         const productCollection = client.db('modernTelecom').collection('product');
 
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken })
+        })
+
 
         // POST
         app.post('/products', async (req, res) => {
@@ -58,12 +85,17 @@ async function run() {
 
         //myorder get api
 
-        app.get('/myitem', async (req, res) => {
+        app.get('/myitem', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = productCollection.find(query);
-            const myItem = await cursor.toArray();
-            res.send(myItem);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = productCollection.find(query);
+                const myItem = await cursor.toArray();
+                res.send(myItem);
+            } else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
         })
 
         app.get('/product/:id', async (req, res) => {
